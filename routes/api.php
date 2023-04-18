bc49aeab41d1fffbec02c13c398c8503004a793a
marxlima <marxmaciel@gmail.com>
2023-04-17 21:49:28 -0300

    criado os controllers

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BilletController;
+use App\Http\Controllers\DocController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WallController;
+use App\Http\Controllers\WarningController;
+use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 Route::get('ping', function() {
     return ['pong' => true];
 });
